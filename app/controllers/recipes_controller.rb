9d53f2b07291cf7937e87e676e51ec251192dac0
AllyJo <gatorally245@gmail.com>
2017-12-28 17:27:37 -0800

    add #index to controller, add view for index

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,28 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all.includes(:tags)
   end
 
+  def create
+    @creator = Recipe.find_by(recipe_params[:creator])
+    @recipe = @creator.recipes.new(recipe_params)
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:name, :instructions, :cooking_time, :cooking_style, :creator)
+  end
+
+
+
 end
