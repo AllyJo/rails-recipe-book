4305a3e91ac2ff852972e9cfe3aa214215fcd331
AllyJo <gatorally245@gmail.com>
2018-01-04 14:45:14 -0800

    add login form and sessions controller

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,15 @@
+module SessionsHelper
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authorize
+    redirect_to login_path, alert: 'You must be logged in to access this page.' if current_user.nil?
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+end
