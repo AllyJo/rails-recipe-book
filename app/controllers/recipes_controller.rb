869ab94b3c5043bbd64f8782706632dc2f842c2c
AllyJo <gatorally245@gmail.com>
2018-01-06 19:56:47 -0800

    edit recipe, ingredients, tags, also add new ingredients and tags when editting recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,44 +1,53 @@
 class RecipesController < ApplicationController
 
   def index
+    # session.delete(:user_id)
     @recipes = Recipe.all.includes(:tags)
   end
+
   def new
     @recipe = Recipe.new
   end
   def create
-    # @user = User.find_by(id: params[:user_id])
-    @recipe = Recipe.includes(:ingredients, :tags).new(recipe_params)
-    if @recipe.save
+    @recipe = Recipe.includes(:ingredients, :tags).create(recipe_params)
+    # if @recipe.creator == current_user
+      if @recipe.save
       redirect_to recipes_path
+      end
     else
-
       @errors = @recipe.errors.full_messages
-      ep @errors
       render 'new'
-    end
+    # end
   end
 
   def show
-    @user = User.find_by(id: params[:user_id])
     @recipe = Recipe.includes(:ingredients, :tags).find(params[:id])
   end
 
   def edit
-    @user = User.find_by(id: params[:user_id])
     @recipe = Recipe.includes(:ingredients, :tags).find(params[:id])
+    @recipe.ingredients.build
+    @recipe.tags.build
   end
 
 
   def update
-    @user = User.find_by(id: params[:user_id])
+    @recipe = Recipe.includes(:ingredients, :tags).find(params[:id])
+    if @recipe.creator == current_user
+      if @recipe.update_attributes(recipe_params)
+        flash[:notice] = "Recipe Updated"
+        redirect_to recipe_path(@recipe)
+      end
+    else
+      @errors = @recipe.errors.full_messages
+      flash[:notice] = "Recipe not updated..."
+      render 'edit'
+    end
   end
 
   private
   def recipe_params
-    params.require(:recipe).permit(:name, :instructions, :cooking_time, :cooking_style)
+    params.require(:recipe).permit(:name, :instructions, :cooking_time, :cooking_style, :creator_id, ingredients_attributes: [:id, :amount, :name], tags_attributes: [:id, :body])
   end
-
-
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ RecipeTag.destroy_all
 RecipeIngredient.destroy_all
 UserRecipe.destroy_all
 
-User.create!(username: 'Tom', email: 'tom@tom.com', password: 'tom')
-User.create!(username: 'Ally', email: 'ally@ally.com', password: 'ally')
+tom = User.create!(username: 'Tom', email: 'tom@tom.com', password: 'tom')
+ally = User.create!(username: 'Ally', email: 'ally@ally.com', password: 'ally')
 chicken = Recipe.create!(name: 'BBQ Chicken', instructions: 'Preheat convection oven to 375. Coat chicken breast with bbq sauce. Place chicken breast on lined baking sheet. Rest for 10 minutes before serving', cooking_time: '45 minutes', cooking_style: 'baked', creator: User.first)
 chicken.ingredients.create!(name: 'chicken breast', amount: '2')
 chicken.ingredients.create!(name: 'Sugar-free bbq sauce', amount: '1/2 cup')
